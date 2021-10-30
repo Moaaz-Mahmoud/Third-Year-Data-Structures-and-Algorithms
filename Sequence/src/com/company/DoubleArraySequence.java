90607e0df94aa997e4ea4b14943fbb7016f7ed52
Moaaz Mahmoud <fasekhs@gmail.com>
2021-10-30 20:31:09 +0200

    Checkpoint

--- Sequence/src/com/company/DoubleArraySequence.java
+++ Sequence/src/com/company/DoubleArraySequence.java
@@ -63,7 +63,8 @@ public class DoubleArraySequence {
         if(size == capacity){
             Double newBuffer[];
             try {
-                newBuffer = new Double[capacity*2];
+                if(capacity == 0) newBuffer = new Double[5];
+                else newBuffer = new Double[capacity*2];
             }
             catch (OutOfMemoryError e){
                 throw e;
@@ -98,12 +99,77 @@ public class DoubleArraySequence {
      * take this sequence beyond its current capacity, then the capacity is increased before adding
      * the new element.
      * @param element  The new element to be added.
-     * @post
+     * @post  A new element has been added to this sequence just before currentIndex.
      * @throws OutOfMemoryError  Indicates insufficient memory to increase the size of this sequence.
      * @implNote An attempt to increase the capacity beyond Integer.MAX_VALUE will cause this sequence
      *           to fail with an arithmetic overflow.
      */
     public void addBefore(double element){
+        if(size == capacity){
+            Double newBuffer[];
+            try {
+                if(capacity == 0) newBuffer = new Double[5];
+                else newBuffer = new Double[capacity*2];
+            }
+            catch (OutOfMemoryError e){
+                throw e;
+            }
 
+            //    [0, 1, ..., curIndex, curIndex+1, ..., size-1]
+            // -> [0, 1, ..., elem, curIndex, curIndex+1, ..., size-1]
+            //No change till curIndex-1
+            for(int i = 0; i < currentIndex; i++){
+                newBuffer[i] = buffer[i];
+            }
+            //Insert `element`
+            newBuffer[currentIndex] = element;
+            //Shift starting from curIndex
+            for(int i = size; i >= currentIndex+1; i--) {
+                newBuffer[i] = buffer[i-1];
+            }
+            buffer = newBuffer;
+        }
+        else{
+            //Shift starting from curIndex
+            for(int i = size; i >= currentIndex; i--) {
+                buffer[i] = buffer[i-1];
+            }
+            //At this point, buffer = [0, 1, ..., curIndex, curIndex, ..., size-1]
+            buffer[currentIndex] = element;
+        }
+        size++;
+    }
+    /**
+     * @param addend  a sequence whose contents will be placed at the end of this sequence.
+     * @pre  The parameter, addend, is not null.
+     * @post The elements from addend have been placed at the end of this sequence. The current element
+     *       of this sequence remains where it was, and the addend is also unchanged.
+     * @throws NullPointerException  Indicates that addend is null.
+     * @throws OutOfMemoryError  Indicates insufficient memory to increase the capacity of this sequence.
+     * @implNote An attempt to increase the capacity beyond Integer.MAX_VALUE will cause this sequence to
+     *           fail with an arithmetic overflow.
+     */
+    public void addAll(DoubleArraySequence addend){
+        if(this.capacity-this.size < addend.size){
+            Double[] newBuffer;
+            try {
+                newBuffer = new Double[this.size + addend.size];
+            }
+            catch (OutOfMemoryError e){
+                throw e;
+            }
+            int i = 0;
+            for(; i < this.size; i++)
+                newBuffer[i] = buffer[i];
+            for(int j = 0; j < addend.size; j++)
+                newBuffer[i++] = addend.buffer[j];
+            this.buffer = newBuffer;
+            this.size += addend.size;
+        }
+        else {
+            for (int i = 0; i < addend.size; i++) {
+                buffer[size++] = addend.buffer[i];
+            }
+        }
     }
 }
